3c9cce88316de738123530cd75cdc8aa3b73da24
LM <130348667+Sotacero@users.noreply.github.com>
2023-04-10 10:39:34 +0200

    Update Pandita.py

--- Pandita.py
+++ Pandita.py
@@ -7,7 +7,7 @@ import re
 import pyperclip
 
 def read_file(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
         return [num.strip() for num in f.readlines()]
 
 def filter_numbers(numbers, criteria):
